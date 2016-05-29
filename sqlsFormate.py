eddb017235f400eba4e4b2cb77f65e2d401f9346
splendone <splendone@gmail.com>
2016-05-29 12:53:17 +0800

    sql bug

--- sqlsFormate.py
+++ sqlsFormate.py
@@ -8,7 +8,7 @@ select *
                trunc(t.billing_date_time)
           from inp_bill_detail t
          where t.item_code = ('{}')
-           and t.billing_date_time between to_date({},'yyyy-mm-dd') and to_date({},'yyyy-mm-dd')
+           and t.billing_date_time between to_date('{}','yyyy-mm-dd') and to_date('{}','yyyy-mm-dd')
          group by patient_id, visit_id, trunc(t.billing_date_time)) TT
  where TT.mount > '{}'
 '''.format(params['itemCode'], params['from'], params['to'], params['mount'])
