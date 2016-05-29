8e44f253947dfb52bcccef2e34f21252b3678f5e
splendone <splendone@gmail.com>
2016-05-29 12:31:00 +0800

    search by item code

--- mainUI.py
+++ mainUI.py
@@ -9,6 +9,10 @@
 from PyQt4 import QtCore, QtGui
 from dialogSetdbUI import Ui_Dialog as Ui_Dialog_Setdb
 from dialogSetItemUI import Ui_Dialog as Ui_Dialog_SetItem
+from sqlsFormate import sqlByItem
+
+DB = None
+CURSOR = None
 
 
 try:
@@ -30,9 +34,10 @@ class Ui_MainWindow(object):
         self.db = None
         self.cursor = None
         
+        
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(891, 550)
+        MainWindow.resize(890, 550)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -41,7 +46,7 @@ class Ui_MainWindow(object):
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.tabWidget = QtGui.QTabWidget(self.centralwidget)
-        self.tabWidget.setGeometry(QtCore.QRect(0, 21, 331, 515))
+        self.tabWidget.setGeometry(QtCore.QRect(10, 10, 320, 510))
         self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
         self.tab_items = QtGui.QWidget()
         self.tab_items.setObjectName(_fromUtf8("tab_items"))
@@ -61,12 +66,14 @@ class Ui_MainWindow(object):
         self.formLayout.setWidget(2, QtGui.QFormLayout.LabelRole, self.label_item)
         self.comboBox_item = QtGui.QComboBox(self.formLayoutWidget)
         self.comboBox_item.setObjectName(_fromUtf8("comboBox_item"))
-        self.comboBox_item.addItem(_fromUtf8(""))
+        self.comboBox_item.addItem(_fromUtf8("120400001"))
+        self.comboBox_item.addItem(_fromUtf8("120400002"))
         self.formLayout.setWidget(2, QtGui.QFormLayout.FieldRole, self.comboBox_item)
         self.label_mount = QtGui.QLabel(self.formLayoutWidget)
         self.label_mount.setObjectName(_fromUtf8("label_mount"))
         self.formLayout.setWidget(3, QtGui.QFormLayout.LabelRole, self.label_mount)
         self.spinBox_mount = QtGui.QSpinBox(self.formLayoutWidget)
+        self.spinBox_mount.setValue(6)
         self.spinBox_mount.setObjectName(_fromUtf8("spinBox_mount"))
         self.formLayout.setWidget(3, QtGui.QFormLayout.FieldRole, self.spinBox_mount)
         self.pushButton = QtGui.QPushButton(self.formLayoutWidget)
@@ -84,7 +91,7 @@ class Ui_MainWindow(object):
         self.tab_others.setObjectName(_fromUtf8("tab_others"))
         self.tabWidget.addTab(self.tab_others, _fromUtf8(""))
         self.tableWidget = QtGui.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(340, 40, 541, 491))
+        self.tableWidget.setGeometry(QtCore.QRect(340, 30, 540, 490))
         self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setRowCount(0)
@@ -109,14 +116,44 @@ class Ui_MainWindow(object):
         self.tabWidget.setCurrentIndex(0)
         QtCore.QObject.connect(self.setdb, QtCore.SIGNAL(_fromUtf8("triggered ()")), self.showDlgSetdb)
         QtCore.QObject.connect(self.setItem, QtCore.SIGNAL(_fromUtf8("triggered ()")), self.showDlgSetItem)
+        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("pressed ()")), self.searchByItem)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         
+    def searchByItem(self):
+        if not self.cursor:
+            self.showDlgSetdb()
+        params = {}
+        #params['itemCode'], params['from'], params['to'], params['mount']
+        params['itemCode'] = str(self.comboBox_item.itemText(self.comboBox_item.currentIndex()))
+        params['from'] = str(self.calendarWidget_from.selectedDate().toString('yyyy-MM-dd'))
+        params['to'] = str(self.calendarWidget_to.selectedDate().toString('yyyy-MM-dd'))
+        params['mount'] = self.spinBox_mount.value()
+        print params
+        sql = sqlByItem(params)
+        print sql
+        r= self.cursor.execute(sql)
+        data = r.fetchall()
+        length=len(data)
+        self.tableWidget.setRowCount(length)
+        for i in range(0, length-1):
+            itemi=data[i]
+            self.tableWidget.setItem(i, 0, QtGui.QTableWidgetItem(itemi[0]))
+            self.tableWidget.setItem(i, 1, QtGui.QTableWidgetItem(itemi[1]))
+            self.tableWidget.setItem(i, 2, QtGui.QTableWidgetItem(itemi[2]))
+            self.tableWidget.setItem(i, 3, QtGui.QTableWidgetItem(itemi[3]))
+        
+        # self.tableWidget.setItem(0, 3, QtGui.QTableWidgetItem('itemi[3]'))
+        
+    
+    
     def showDlgSetdb(self):
         Dialog = QtGui.QDialog()
         ui = Ui_Dialog_Setdb(self)
         ui.setupUi(Dialog)
         Dialog.exec_()
         print 'dlg set db closed...'
+        CURSOR = self.cursor
+        DB = self.db
         
     def showDlgSetItem(self):
         Dialog = QtGui.QDialog()
@@ -129,7 +166,8 @@ class Ui_MainWindow(object):
         self.label_from.setText(_translate("MainWindow", "From", None))
         self.label_to.setText(_translate("MainWindow", "To", None))
         self.label_item.setText(_translate("MainWindow", "Item", None))
-        self.comboBox_item.setItemText(0, _translate("MainWindow", "120400001", None))
+        # self.comboBox_item.setItemText(0, _translate("MainWindow", "120400001", None))
+        # self.comboBox_item.setItemText(1, _translate("MainWindow", "120400002", None))
         self.label_mount.setText(_translate("MainWindow", "Mount > ", None))
         self.pushButton.setText(_translate("MainWindow", "Search", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_items), _translate("MainWindow", "Items", None))
@@ -145,13 +183,23 @@ class Ui_MainWindow(object):
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar", None))
     
 
-
+class MyWindow(QtGui.QMainWindow):
+    
+    def closeEvent(self,event):
+        if CURSOR:
+            CURSOR.close()
+        if DB:
+            DB.close()
+        print 'close db/cursor ...'
+    
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
+    MainWindow = MyWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    
     sys.exit(app.exec_())
+    # ui.closeDBConnect()
 
---
+++ sqlsFormate.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+def sqlByItem(params):
+    sql = '''
+select *
+  from (select patient_id,
+               visit_id,
+               SUM(t.amount) mount,
+               trunc(t.billing_date_time)
+          from inp_bill_detail t
+         where t.item_code = ('{}')
+           and t.billing_date_time between to_date({},'yyyy-mm-dd') and to_date({},'yyyy-mm-dd')
+         group by patient_id, visit_id, trunc(t.billing_date_time)) TT
+ where TT.mount > '{}'
+'''.format(params['itemCode'], params['from'], params['to'], params['mount'])
+    return sql
